9b216ea7d139220121c7757099c06605478509c2
Balazs Szakmary <szakib@gmail.com>
2011-02-22 13:41:49 +0000

    Some classes
    git-svn-id: https://szakicomp:8443/svn/ThesisCode@11 b5b39435-3fcd-5447-9be7-4ceb1b0ff48a

---
+++ GEM/Individual.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GEM
+{
+    /// <summary>
+    /// An individual in the population of the genetic algorithm
+    /// </summary>
+    public class Individual
+    {
+        /// <summary>
+        /// The genes of the individual
+        /// </summary>
+        private GeneSet Genes
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// The dataset that the individual represents
+        /// </summary>
+        private DataSet DataSet
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    }
+}
--- GEM/MainForm.Designer.cs
+++ GEM/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace GEM
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
